14f70dc3edff7226b9f0ac1fe9f0c8944fad674d
John Wu <webmaster@leapoahead.com>
2014-12-05 10:31:58 +0800

    Add Doc

--- Assetee/Initializers/UINavigationBarCustomizeInitializer.m
+++ Assetee/Initializers/UINavigationBarCustomizeInitializer.m
@@ -3,7 +3,9 @@
 //  Assetee
 //
 //  Created by Wu Di on 12/5/14.
-//  Copyright (c) 2014 吴迪（子回）. All rights reserved.
+//  Copyright (c) 2014 吴迪（子回）. All rights reserved.\
+//
+//  Customize the color of UINavigationBar
 //
 
 #import "UINavigationBarCustomizeInitializer.h"
